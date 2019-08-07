1399df30f6d338dbe6cc0f71873a91ff2cb11787
mark-renrgyx <53844281+mark-renrgyx@users.noreply.github.com>
2019-08-07 14:42:01 -0400

    finished -- with fixed values

---
+++ Sum.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+// Assignment 2
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		int first = 1;
+		int second = 2;
+		
+		System.out.println(getSum(first, second));
+	}
+	
+	public static int getSum (int a, int b) {
+		return a - -b;
+	}
+}
